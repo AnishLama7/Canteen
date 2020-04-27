7cb0d048b1b846f4317d1bb6985fccab7ab2c67e
AnishLama7 <64069180+AnishLama7@users.noreply.github.com>
2020-04-27 23:39:50 +0545

    STYLE GAREKO
    ...

--- canteen/cms/login.php
+++ canteen/cms/login.php
@@ -1,36 +1,47 @@
 <?php 
-	require_once '../includes/init.php';
-	$title = 'Online';
-	require_once 'templates/header.php';
- ?>
+require_once '../includes/init.php';
+$title = 'Online';
+require_once 'templates/header.php';
+?>
+<style type="text/css">
+	h1{
+		color:darkorange;
+	}
+	.but{
+		background-color:darkorange;
+	}
+	.bod{
+		background-image: url("../images/4.jpg");
+		background-repeat: no-repeat;
+/*		background-size: cover;
+*/		background-attachment: fixed;
+background-size: 100% 100%;
 
-<div class="col-4 mx-auto">
-	<div class="col-12 bg-white my-3">
-		<div class="row">
-			<div class="col-12 text-center">
-				<h1>Canteen Login</h1>
-			</div>
-			<?php include_once 'templates/message.php'; ?>
-			<div class="col-12">
-				<form method="POST" action="<?php echo url('cms/login_check.php') ?>">
-					<div class="form-group">
-						<label for="username">Username/Email</label>
-						<input type="text" name="username" id="username" class="form-control" required >
-					</div>
-					<div class="form-group">
-						<label for="password">Password</label>
-						<input type="password" name="password" id="upassword" class="form-control" required >
-					</div>
-					<div class="form-group">
-						<button type="submit" class="btn btn-primary"> log In</button>
-					</div>
-				</form>
-			</div>
+}
+</style>
+<body class="bg-secondary bod">
+	<div class="mx-auto bg-dark p-5 mt-5 col-lg-4 col-md-8 col-12" style="opacity:0.8">
+		<h1 class="text-center">Canteen Login</h1>
+		<hr color="orange">
+		<?php include_once 'templates/message.php'; ?>
+		<div class="text-white">
+			<form method="POST" action="<?php echo url('cms/login_check.php') ?>">
+				<div class="form-group">
+					<label for="username">Username/Email:</label>
+					<input type="text" name="username" id="username" class="form-control" required >
+				</div>
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" name="password" id="upassword" class="form-control" required >
+				</div>
+				<div class="form-group text-center" >
+					<button type="submit" class="btn btn-secondary mt-2 but font-weight-bold"> Log In</button>
+				</div>
+			</form>
 		</div>
 	</div>
-</div>
+</body>
 
 <?php 
-	require_once 'templates/footer.php';
- ?>
-		
+require_once 'templates/footer.php';
+?>
