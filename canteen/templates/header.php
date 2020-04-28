dffa8f4c5ed824340aa3458e71635df0d4dafe41
AnishLama7 <64069180+AnishLama7@users.noreply.github.com>
2020-04-28 18:13:01 +0545

    LINK MILAKO
    link tala mathi milako matra ho nothing added nothing deleted

--- canteen/templates/header.php
+++ canteen/templates/header.php
@@ -8,8 +8,9 @@
 	
 	<link rel="stylesheet" type="text/css" href="<?php echo url('css/bootstrap.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo url('css/cms.css')?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo url('css/style.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo url('css/all.css')?>">
+	
+	<!-- 	<link rel="stylesheet" type="text/css" href="<?php echo url('css/style.css')?>"> -->	
 <!-- 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="sty.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
