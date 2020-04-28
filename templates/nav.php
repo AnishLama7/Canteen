af686d2be419d0cd8e3c7cf4655f740f4d212f31
sarisha5 <sarisha.ghimire5@gmail.com>
2020-04-27 20:37:52 -0600

    deleted navbar

--- templates/nav.php
+++ templates/nav.php
@@ -31,8 +31,13 @@
 								contact
 							</a>
 						</li>
+						<li class="ml-3 my-2">
+							<button class="bg-dark btn btn-primary" data-toggle="modal" data-target="#myModal">Log in</button>
+						</li>
+						<li class="ml-3 my-2">
+							<a href="signup.php" target="_blank"><button class="bg-dark btn btn-primary"> Sign Up </button></a>
+						</li>
 						<!-- style="color:rgba(255,255,255,.5); -->
-						<?php require 'templates/navbar.php'; ?>
 					</ul>
 				</div>		
 			</nav>
