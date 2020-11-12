47a8831d4cd600346f73c0b04dd16f1dde34623c
ZeluThapa7 <64134234+ZeluThapa7@users.noreply.github.com>
2020-11-13 00:29:52 +0545

    Update nav.php
    login ra sign up corner ma lageko, link ko font size badako, margin rakhera gap badhako between home menu etc

--- canteen/templates/nav.php
+++ canteen/templates/nav.php
@@ -1,48 +1,56 @@
 <style type="text/css">
 	a{
-		font-size: 19px;
+		font-size: 22px;
 	}
 	
+
+
+
 </style>
 <!-- nav -->
-	<section class="bg-dark">
-		<div class="container">
-			<nav class="navbar navbar-expand-md navbar-dark">
-				<!-- Brand/logo -->
-				<a class="navbar-brand" href="home.php">
-					<img src="images/newlogo.png" width="130px" height="60px">
-				</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="collapsibleNavbar">
-					<ul class="navbar-nav">
+<section class="bg-dark">
+	<div class="container-fluid">
+		<nav class="navbar navbar-expand-md navbar-dark">
+			<!-- Brand/logo -->
+			<a class="navbar-brand" href="home.php">
+				<img src="images/newlogo.png" width="140px" height="65px">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="collapsibleNavbar">
 
-						<li class="nav-item">
-							<a class="nav-link" href="<?php echo url('home.php'); ?>">Home</a>
-						</li>
+				<ul class="navbar-nav">
 
-						<li class="nav-item">
-							<a class="nav-link" href="<?php echo url('about.php'); ?>">About Us</a>
-						</li>
+					<li class="nav-item mr-3">
+						<a class="nav-link" href="<?php echo url('home.php'); ?>">Home</a>
+					</li>
 
-						<li class="nav-item">
-							<a class="nav-link" href="<?php echo url('menu.php'); ?>">Menu</a>
-						</li>
+					<li class="nav-item mr-3">
+						<a class="nav-link" href="<?php echo url('about.php'); ?>">About Us</a>
+					</li>
 
-						<li class="nav-item">
-							<a class="nav-link" href="<?php echo url('contact.php'); ?>">
-								Contact
-							</a>
-						</li>
-					</ul>
+					<li class="nav-item mr-3">
+						<a class="nav-link" href="<?php echo url('menu.php'); ?>">Menu</a>
+					</li>
 
-					<ul class="nav navbar-nav navbar-right">
-					  <li><a href="<?php echo url('cms/login.php'); ?>" class="text-secondary"><i class="fas fa-sign-in-alt text-secondary" ></i>Login</a></li>
-				      <li><a href="<?php echo url('signup.php'); ?>" class="text-secondary"><i class="fas fa-user text-secondary"></i> Sign Up</a></li> 
-				    </ul>
-					
-				</div>		
-			</nav>
-		</div>
-	</section>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo url('contact.php'); ?>">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</div>
+
+
+<!-- 			<div class="collapse navbar-collapse" id="collapsibleNavbar">
+ -->
+				<ul class="nav navbar-nav justify-content-end">
+
+					<li><a href="<?php echo url('cms/login.php'); ?>" class="text-secondary mr-3"><i class="fas fa-sign-in-alt text-secondary " ></i>Login</a></li>
+					<li><a href="<?php echo url('signup.php'); ?>" class="text-secondary"><i class="fas fa-user text-secondary"></i> Sign Up</a></li> 
+				</ul>
+<!-- 			</div>		
+ -->		</nav>
+	</div>
+</section>
