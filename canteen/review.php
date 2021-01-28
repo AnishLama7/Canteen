ef43f26ebc394435ada1b265fa64a58bea078db6
sarisha5 <sarisha.ghimire5@gmail.com>
2021-01-28 20:24:03 +0545

    made changes in revieww part

--- canteen/review.php
+++ canteen/review.php
@@ -7,89 +7,71 @@
   include 'templates/header.php';
  ?>
 
- <div class="container">
-     <div class="rating-review">
-         <div class="tri table-flex">
-             <table>
-                 <tbody>
-                     <tr>
-                         <td>
-                             <div class="rnb rvl1">
-                                 <h3>1.5/5</h3>
-                             </div>
-                             <div class="pdt rate">
-                                 <div class="pro-rating">
-                                     <div class="clearfix rating mart8">
-                                         <div class="rating-star">
-                                             <div class="grey-star"></div>
-                                             <div class="filled-star" style="width: 60%">
-                                                </div>     
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>
-                             <div class="rnrn">
-                                 <p class="rars">No Review</p>
-                             </div>
-                         </td>
-                         <td>
-                             <div class="rph">
-                                 <div class="rnpb">
-                                     <label>5<i class="fa fa-star"></i></label>
-                                     <div class="ropb">
-                                        <div class="ripb" style="width: 20%"></div>
-                                     </div>
-                                     <div class="label">(1)</div>
-                                 </div>
+ <div class="container my-4">
+     <div class="row">
+      <div class="col-sm-4">
+         <div class="rnb rvl1">
+            <h3>1.5/5</h3>
+        </div>
+        <div class="pdt rate">
+            <div class="pro-rating">
+                <div class="clearfix rating mart8">
+                    <div class="rating-star">
+                        <div class="grey-star"></div>
+                        <div class="filled-star" style="width: 60%">
+                    </div>     
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="rnrn">
+        <p class="rars">No Review</p>
+    </div>
+    </div>
 
-                                  <div class="rnpb">
-                                     <label>4<i class="fa fa-star"></i></label>
-                                     <div class="ropb">
-                                        <div class="ripb" style="width: 20%"></div>
-                                     </div>
-                                     <div class="label">(10)</div>
-                                 </div>
+    <div class="col-sm-4">
+        <div class="progress">
+            <label>5&nbsp;</label>
+          <div class="progress-bar bg-warning" role="progressbar" style="width: 5%" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
+        </div><br>
+        <div class="progress">
+            <label>4&nbsp;</label>
+          <div class="progress-bar bg-warning" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div><br>
+        <div class="progress">
+            <label>3&nbsp;</label>
+          <div class="progress-bar bg-warning" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+        </div><br>
+        <div class="progress">
+            <label>2&nbsp;</label>
+          <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+        </div><br>
+        <div class="progress">
+            <label>1&nbsp;</label>
+          <div class="progress-bar bg-warning" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+    </div>
 
-                                  <div class="rnpb">
-                                     <label>3<i class="fa fa-star"></i></label>
-                                     <div class="ropb">
-                                        <div class="ripb" style="width: 20%"></div>
-                                     </div>
-                                     <div class="label">(15)</div>
-                                 </div>
 
-                                  <div class="rnpb">
-                                     <label>2<i class="fa fa-star"></i></label>
-                                     <div class="ropb">
-                                        <div class="ripb" style="width: 20%"></div>
-                                     </div>
-                                     <div class="label">(11)</div>
-                                 </div>
+    <div class="col-sm-4">
+        <div class="col text-center">
+            <h3>Post a review</h3><br>
+            <button type="button" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#myModal">Add Review</button>
 
-                                  <div class="rnpb">
-                                     <label>1<i class="fa fa-star"></i></label>
-                                     <div class="ropb">
-                                        <div class="ripb" style="width: 40%"></div>
-                                     </div>
-                                     <div class="label">(13)</div>
-                                 </div>
-                             </div>
-                         </td>
-                         <td>
-                             <div class="rvb">
-                                 <p>Please Review this item</p>
-                                 <button class="rbtn opmd">Add Review</button>
-                             </div>
-                         </td>
-                     </tr>
-                 </tbody>
-             </table>
-             <div class="review-modal">
-                 <div class="review-bg"></div>
-                 <div class="rmp">
-                     <div class="rpc">
-                         <span><i class="far fa-times"></i></span>
-                     </div>
+              <!-- Modal -->
+              <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog">
+                
+                  <!-- Modal content-->
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h4 class="modal-title">Rate/Add your review...</h4>
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+
+                      <form>
+                        <div class="rmp">
                      <div class="rps" align="center">
                          <i class="fa fa-star" data-index="0" style="display:none;"></i>
                          <i class="fa fa-star" data-index="1"></i>
@@ -100,16 +82,29 @@
                      </div>
                      <input type="hidden" value="" class="starRateV">
                      <input type="hidden" value="" class="rateDate">
-                     <div class="rptf" align="center">
-                         <textarea class="rateMsg" placeholder="Post your review for this item.."></textarea>
-                     </div>
-                     <div class="rate-error" align="center"></div>
-                     <div class="rpsb" align="center">
-                         <button class="rpbtn">Post Review</button>
-                     </div>
+                     <textarea class="form-control" id="msg" placeholder="Add your review for this item.."></textarea><br>
+                            <button class="btn btn-success">Post Review</button>
                  </div>
+                      </form>
+
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+        
+                </div>
+              </div>
+        </div>
+    </div>
+
+    <div class="review-modal">
+                 <div class="review-bg"></div>
              </div>
          </div>
+
+         <br><br><hr><br>
+
          <div class="bri">
              <div class="uscm">
                  <div class="uscms-secs">
@@ -123,7 +118,7 @@
                                      <div class="clearfix rating mart8">
                                          <div class="rating-star">
                                              <div class="grey-star"></div>
-                                             <div class="filled-star" style="width: 60%">
+                                             <div class="filled-star" style="width: 50%">
                                                 </div>     
                                          </div>
                                      </div>
@@ -137,10 +132,17 @@
                           <div class="us-rn">
                               <p><i>By <span class="cmnm">Bright</span> on <span class="cmdt">Jan 21 2020</span></i></p>
                           </div>
-                     </div>
+                     </div> 
                  </div>
              </div>
-         </div>
-     </div>
+
+
+
+    </div>
  </div>
+
+ <?php include 'templates/footer.php'?>
+
+ 
+     
 
